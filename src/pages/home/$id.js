4f44fdfd2b71d3b75ac14af46d4d1caf45013903
ybzjsxh <musiq841@gmail.com>
2019-11-04 17:30:58 +0800

    update

--- src/pages/home/$id.js
+++ src/pages/home/$id.js
@@ -3,9 +3,19 @@ import marked from 'marked';
 import hljs from 'highlight.js';
 
 import './article.less';
+import 'highlight.js/styles/atom-one-dark.css';
 
 marked.setOptions({
+  renderer: new marked.Renderer(),
   highlight: code => hljs.highlightAuto(code).value,
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false,
 });
 
 export default props => {
@@ -13,13 +23,12 @@ export default props => {
   const onOpenChange = () => {
     props.history.goBack();
   };
-  const output = marked(`**function a () {${props.match.params.id}}**`);
   return (
     <>
       <NavBar icon={<Icon type="left" />} onLeftClick={onOpenChange}>
         {props.match.params.id}
       </NavBar>
-      <div dangerouslySetInnerHTML={{ __html: output }} />
+      <div dangerouslySetInnerHTML={{ __html: marked('```js \n function () {} \n ```') }} />
     </>
   );
 };
