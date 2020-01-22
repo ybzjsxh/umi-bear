148809f26b2bc628b3af4abba0929241610ffd82
ybzjsxh <musiq841@gmail.com>
2020-01-22 23:56:58 +0800

    feat(www): update dockerfile

--- src/pages/article/components/comment.jsx
+++ src/pages/article/components/comment.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState /* useEffect */ } from 'react';
 import { Button, TextareaItem, Toast } from 'antd-mobile';
 
 import styles from './comment.less';
